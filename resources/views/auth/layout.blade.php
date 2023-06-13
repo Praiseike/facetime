b33fb960b990c2a7278e37a7cc54db50ba6211b4
praiseike <praiseike123@gmail.com>
2023-06-13 16:50:34 +0100

    something

--- resources/views/auth/layout.blade.php
+++ resources/views/auth/layout.blade.php
@@ -3,11 +3,13 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>@yield('title')</title>
     @vite('resources/css/app.css')
 </head>
 <body class="bg-slate-900">
-    <div class="w-[70%] mx-auto h-screen flex flex-col justify-center items-center text-slate-300 ">
+    <div class="w-full sm:md:w-[70%] mx-auto h-screen flex flex-col justify-center items-center text-slate-300 ">
         @yield('content')
     </div>
 </body>
