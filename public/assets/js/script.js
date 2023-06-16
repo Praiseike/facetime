dff87a3d07fdbbc2292226b825f138e942b59b74
praiseike <praiseike123@gmail.com>
2023-06-16 22:52:42 +0100

    fixed code error

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -270,6 +270,7 @@ const updateUsers = (msg) => {
     const userList = document.querySelector('#user-list');
     const addUser = user => {
         if(user.id != msg.target){
+            userList.innerHTML = '';
             userList.innerHTML += `
                 <li id="call" data-id="${user.id}" " class="rounded-lg p-4 m-4 w-[10rem] h-[10rem] flex justify-center space-y-4 cursor-pointer bg-[#64748b99] flex-col items-center border border-slate-600">
                     <div class="bg-teal-400 rounded-full w-14 h-14 flex items-center justify-center text-white font-bold text-2xl">
@@ -288,7 +289,7 @@ const updateUsers = (msg) => {
         });        
     }
     // update onclick handlers for all #call buttons
-    document.querySelectorAll('#call').forEach((btn) => btn.addEventListener('click',call));
+    document.querySelectorAll('#call').forEach((btn) => (msg.target != btn.getAttribute('data-id')) && btn.addEventListener('click',call));
 
 }
 
